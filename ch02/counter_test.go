57f1f6cde342600ee5428355eca1a98b0ea8aafa
smile <mad024039@gmail.com>
2025-04-22 15:11:58 +0800

    refactor(counter_test)

--- ch02/counter_test.go
+++ ch02/counter_test.go
@@ -8,6 +8,24 @@ import (
 func TestCounter(t *testing.T) {
 	var counter int64
 	var wg sync.WaitGroup
+	for range 64 {
+		wg.Add(1)
+		go func() {
+			for range 1000000 {
+				counter++
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if counter != 64000000 {
+		t.Errorf("Expected 64000000, got %d", counter)
+	}
+}
+
+func TestCounterWithMutex(t *testing.T) {
+	var counter int64
+	var wg sync.WaitGroup
 	var mu sync.Mutex
 	for range 64 {
 		wg.Add(1)
